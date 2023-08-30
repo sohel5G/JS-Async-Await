150ef6d864b0a013d268fb1268af61dbc48baf13
sohel5G <contactsohelrana@gmail.com>
2023-08-30 16:48:42 +0600

    Module 35 done

--- interval.js
+++ interval.js
@@ -3,13 +3,28 @@ console.log(2);
 console.log(3);
 
 let varName = 1;
-setInterval(() => {
+
+const setIntervalId = setInterval(() => {
     console.log( varName++ );
 }, 1000);
+
+
+const settimeId = setTimeout(() => {
+    clearInterval(setIntervalId) 
+}, 5000);
+
+
+// clearTimeout(settimeId);
 
 console.log(5);
 console.log(6);
 console.log(7);
+
+
+
+
+
+
 
 
 
---
+++ try-catch.js
@@ -0,0 +1,41 @@
+
+
+function checkAge() {
+    const ageInput = document.getElementById('age-input');
+    const ageInputText = ageInput.value;
+
+
+
+    console.log(ageInputText);
+    try {
+        console.log(nothing); //this variable make error
+        throw nothing;
+    } catch (err) {
+        console.log('ERR:: ', err);
+    } finally {
+        console.log('This code will run of-course')
+    }
+
+
+    console.log('I am a simple text');
+}
+
+
+
+
+
+
+
+const ageBtn = document.getElementById('btn-sub');
+
+
+
+
+
+
+
+
+
+
+
+
